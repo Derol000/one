957b6d4f6d1367fd74263933ae98a966aa525cbb
Derol000 <121206145+Derol000@users.noreply.github.com>
2022-12-22 10:09:59 +0900

    Add files via upload

---
+++ Сапёр/Minesweeper/Forms/Minesweeper.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Minesweeper
+{
+    partial class Minesweeper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.beginnerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.intermediateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.expertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblMinesLeft = new System.Windows.Forms.Label();
+            this.showMinesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showHintsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showPercentagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showCellLocationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showCellHighlightsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hintsAndTipsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.newGameToolStripMenuItem,
+            this.hintsAndTipsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(792, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitToolStripMenuItem.Text = "Выйти из игры";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.beginnerToolStripMenuItem,
+            this.intermediateToolStripMenuItem,
+            this.expertToolStripMenuItem});
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(162, 20);
+            this.newGameToolStripMenuItem.Text = "Выбор уровня сложности";
+            // 
+            // beginnerToolStripMenuItem
+            // 
+            this.beginnerToolStripMenuItem.Name = "beginnerToolStripMenuItem";
+            this.beginnerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.beginnerToolStripMenuItem.Text = "Лёгкий";
+            this.beginnerToolStripMenuItem.Click += new System.EventHandler(this.beginnerToolStripMenuItem_Click);
+            // 
+            // intermediateToolStripMenuItem
+            // 
+            this.intermediateToolStripMenuItem.Name = "intermediateToolStripMenuItem";
+            this.intermediateToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.intermediateToolStripMenuItem.Text = "Средний";
+            this.intermediateToolStripMenuItem.Click += new System.EventHandler(this.intermediateToolStripMenuItem_Click);
+            // 
+            // expertToolStripMenuItem
+            // 
+            this.expertToolStripMenuItem.Name = "expertToolStripMenuItem";
+            this.expertToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.expertToolStripMenuItem.Text = "Сложный";
+            this.expertToolStripMenuItem.Click += new System.EventHandler(this.expertToolStripMenuItem_Click);
+            // 
+            // lblMinesLeft
+            // 
+            this.lblMinesLeft.AutoSize = true;
+            this.lblMinesLeft.Location = new System.Drawing.Point(16, 38);
+            this.lblMinesLeft.Name = "lblMinesLeft";
+            this.lblMinesLeft.Size = new System.Drawing.Size(81, 13);
+            this.lblMinesLeft.TabIndex = 3;
+            this.lblMinesLeft.Text = "Мин осталось:";
+            // 
+            // showMinesToolStripMenuItem
+            // 
+            this.showMinesToolStripMenuItem.Name = "showMinesToolStripMenuItem";
+            this.showMinesToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.showMinesToolStripMenuItem.Text = "Показать мины";
+            this.showMinesToolStripMenuItem.Click += new System.EventHandler(this.showMinesToolStripMenuItem_Click);
+            // 
+            // showHintsToolStripMenuItem
+            // 
+            this.showHintsToolStripMenuItem.Name = "showHintsToolStripMenuItem";
+            this.showHintsToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.showHintsToolStripMenuItem.Text = "Показать подсказки";
+            this.showHintsToolStripMenuItem.Click += new System.EventHandler(this.showHintsToolStripMenuItem_Click);
+            // 
+            // showPercentagesToolStripMenuItem
+            // 
+            this.showPercentagesToolStripMenuItem.Name = "showPercentagesToolStripMenuItem";
+            this.showPercentagesToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.showPercentagesToolStripMenuItem.Text = "Показать шансы  %";
+            this.showPercentagesToolStripMenuItem.Click += new System.EventHandler(this.showPercentagesToolStripMenuItem_Click);
+            // 
+            // showCellLocationsToolStripMenuItem
+            // 
+            this.showCellLocationsToolStripMenuItem.Name = "showCellLocationsToolStripMenuItem";
+            this.showCellLocationsToolStripMenuItem.Size = new System.Drawing.Size(243, 22);
+            this.showCellLocationsToolStripMenuItem.Text = "Показать расположение ячеек";
+            this.showCellLocationsToolStripMenuItem.Click += new System.EventHandler(this.showCellLocationsToolStripMenuItem_Click);
+            // 
+            // showCellHighlightsToolStripMenuItem
+            // 
+            this.showCellHighlightsToolStripMenuItem.Name = "showCellHighlightsToolStripMenuItem";
+            this.showCellHighlightsToolStripMenuItem.Size = new System.Drawing.Size(247, 22);
+            this.showCellHighlightsToolStripMenuItem.Text = "Выделить окружающие ячейки";
+            this.showCellHighlightsToolStripMenuItem.Click += new System.EventHandler(this.showCellHighlightsToolStripMenuItem_Click);
+            // 
+            // hintsAndTipsToolStripMenuItem
+            // 
+            this.hintsAndTipsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showMinesToolStripMenuItem,
+            this.showHintsToolStripMenuItem,
+            this.showPercentagesToolStripMenuItem,
+            this.showCellLocationsToolStripMenuItem,
+            this.showCellHighlightsToolStripMenuItem});
+            this.hintsAndTipsToolStripMenuItem.Name = "hintsAndTipsToolStripMenuItem";
+            this.hintsAndTipsToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.hintsAndTipsToolStripMenuItem.Text = "Читы";
+            this.hintsAndTipsToolStripMenuItem.Click += new System.EventHandler(this.hintsAndTipsToolStripMenuItem_Click);
+            // 
+            // Minesweeper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(792, 620);
+            this.Controls.Add(this.lblMinesLeft);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Minesweeper";
+            this.Text = "Minesweeper";
+            this.Click += new System.EventHandler(this.Minesweeper_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Minesweeper_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Minesweeper_MouseMove);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem beginnerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem intermediateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem expertToolStripMenuItem;
+        private System.Windows.Forms.Label lblMinesLeft;
+        private System.Windows.Forms.ToolStripMenuItem hintsAndTipsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showMinesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showHintsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showPercentagesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showCellLocationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showCellHighlightsToolStripMenuItem;
+    }
+}
+
---
+++ Сапёр/Minesweeper/Forms/Minesweeper.cs
@@ -0,0 +1,323 @@
+﻿using Minesweeper.Core;
+using Minesweeper.Core.Boards;
+using System;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Minesweeper : Form
+    {
+        public Board _board { get; private set; }
+        public GameMode _mode { get; private set; }
+        public Random _random { get; private set; }
+        public int BOARD_WIDTH { get; private set; }
+        public int BOARD_HEIGHT { get; private set; }
+        public int NUM_MINES { get; private set; }
+
+        public Minesweeper()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+
+            _random = new Random();
+
+            _mode = GameMode.Intermediate;
+            StartGame();
+        }
+
+        /// <summary>
+        /// Starts a new game.
+        /// </summary>
+        private void StartGame()
+        {
+            GetBoardSize();
+
+            _board = new Board(this, BOARD_WIDTH, BOARD_HEIGHT, NUM_MINES);
+            _board.SetupBoard();
+            _board.PlaceMines();
+
+            // Dynamically resize the board to fit
+            Width = (BOARD_WIDTH * Board.CellSize) + (int)(Board.CellSize * 1.5);
+            Height = (BOARD_HEIGHT * Board.CellSize) + Board.CellSize * 4;
+
+            UpdateMinesRemaining();
+
+            showMinesToolStripMenuItem.Checked = false;
+            showCellLocationsToolStripMenuItem.Checked = false;
+            showPercentagesToolStripMenuItem.Checked = false;
+        }
+
+        /// <summary>
+        /// Updates the number of mines remaining on the UI.
+        /// </summary>
+        /// <param name="numberOfMines"></param>
+        public void UpdateMinesRemaining()
+        {
+            lblMinesLeft.Text = $"Мин осталось: {_board.NumMinesRemaining}";
+        }
+
+        /// <summary>
+        /// Size of the board is determined by the GameMode.
+        /// </summary>
+        /// <returns></returns>
+        private void GetBoardSize()
+        {
+            switch (_mode)
+            {
+                case GameMode.Beginner:
+                    BOARD_WIDTH = 8;
+                    BOARD_HEIGHT = 8;
+                    NUM_MINES = 10;
+                    break;
+                case GameMode.Intermediate:
+                    BOARD_WIDTH = 16;
+                    BOARD_HEIGHT = 16;
+                    NUM_MINES = 40;
+                    break;
+                case GameMode.Expert:
+                    BOARD_WIDTH = 30;
+                    BOARD_HEIGHT = 16;
+                    NUM_MINES = 99;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Returns the cell that was clicked on. Or null if the click was outside of the bounds of the 
+        /// game board.
+        /// </summary>
+        /// <param name="mouseArgs"></param>
+        /// <returns></returns>
+        private Cell GetCellFromMouseEvent(MouseEventArgs mouseArgs)
+        {
+            // Game is in an over state - do not register clicks on the board.
+            if (_board != null && _board.GameOver)
+            {
+                return null;
+            }
+
+            var clickedX = mouseArgs.X - (Board.CellSize / 2);
+            var clickedY = mouseArgs.Y - (Board.CellSize * 2);
+
+            var cellX = clickedX / Board.CellSize;
+            var cellY = clickedY / Board.CellSize;
+
+            // Check for out of bounds:
+            if (clickedX < 0 || clickedY < 0 || cellX < 0 || cellY < 0 || cellX >= _board.Width || cellY >= _board.Height)
+            {
+                return null;
+            }
+
+            return _board.Cells[cellX, cellY];
+        }
+
+        /// <summary>
+        /// Handles click events on the form for opening/flagging cells.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Minesweeper_Click(object sender, EventArgs e)
+        {
+            var mouseArgs = (MouseEventArgs)e;
+
+            var cell = GetCellFromMouseEvent(mouseArgs);
+            if (cell == null)
+                return;
+
+            switch (mouseArgs.Button)
+            {
+                case MouseButtons.Left:
+                    // Left click opens the cell:
+                    cell.OnClick();
+                    AfterClick();
+                    break;
+                case MouseButtons.Right:
+                    // Right click places a flag:
+                    if (cell.Closed)
+                    {
+                        cell.OnFlag();
+                        AfterClick();
+                    }
+                    break;
+                default: 
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Updates the UI after the user has clicked
+        /// </summary>
+        private void AfterClick()
+        {
+            _board.UpdateCells();
+            _board.CheckForWin();
+
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Paints the game board.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Minesweeper_Paint(object sender, PaintEventArgs e)
+        {
+            if (_board != null)
+            {
+                _board.Painter?.Paint(e.Graphics);
+            }
+        }
+
+        /// <summary>
+        /// Handles the user wanting to quit the application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var response = MessageBox.Show("Ты хочешь выйти из игры?", "Выйти из игры?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+            if (response == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        /// <summary>
+        /// Reveals (or hides) the locations of the mines.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void showMinesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _board.ShowMines = !_board.ShowMines;
+            showMinesToolStripMenuItem.Checked = _board.ShowMines;
+
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Reveals (or hides) the computers tips of which cells are safe/mines.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void showHintsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _board.ShowHints = !_board.ShowHints;
+            _board.UpdateCells();
+            showHintsToolStripMenuItem.Checked = _board.ShowHints;
+
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Reveals (or hides) the percentages of each cell being a mine.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void showPercentagesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _board.ShowPercentage = !_board.ShowPercentage;
+            _board.UpdateCells();
+            showPercentagesToolStripMenuItem.Checked = _board.ShowPercentage;
+
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Reveals (or hides) the cell location co-ordinates.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void showCellLocationsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _board.ShowLocation = !_board.ShowLocation;
+            showCellLocationsToolStripMenuItem.Checked = _board.ShowLocation;
+
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Toggles whether to highlight the cells surrounding.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void showCellHighlightsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _board.ShowCellHighlights = !_board.ShowCellHighlights;
+            showCellHighlightsToolStripMenuItem.Checked = _board.ShowCellHighlights;
+
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Starts a new beginner game.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void beginnerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfirmNewGame(GameMode.Beginner);
+        }
+
+        /// <summary>
+        /// Starts a new intermediate game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void intermediateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfirmNewGame(GameMode.Intermediate);
+        }
+
+        /// <summary>
+        /// Starts a new expert game.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void expertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfirmNewGame(GameMode.Expert);
+        }
+
+        /// <summary>
+        /// Confirms that the user wants to start a new game.
+        /// </summary>
+        /// <returns></returns>
+        private void ConfirmNewGame(GameMode mode)
+        {
+            var response = MessageBox.Show("Хочешь начать новую игру?", "Начать новую игру", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+            if (response == DialogResult.Yes)
+            {
+                _mode = mode;
+                StartGame();
+            }
+        }
+
+        /// <summary>
+        /// Event handler for when the mouse is moved.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Minesweeper_MouseMove(object sender, MouseEventArgs e)
+        {
+            var cell = GetCellFromMouseEvent(e);
+            _board.Painter.HoveredCell = cell;
+            Cursor = cell != null && cell.Closed ? Cursors.Hand : Cursors.Default;
+
+            if (cell != null)
+            {
+                _board.UpdateCells();
+            }
+
+            Invalidate();
+        }
+
+        private void hintsAndTipsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
